f54b7f741a975918cfe9d7ad1d7ee81156e54ede
taikarir <69635722+taikarir@users.noreply.github.com>
2022-02-21 14:41:33 -0500

    Added js file for word list

--- sketch.js
+++ sketch.js
@@ -1,6 +1,6 @@
 console.log("Welcome to Wordle.6!\nTo choose a new secret word, type 'SECRET_WORD = randomizeWord();'\nTo select a secret word, type 'SECRET_WORD = selectWord('YourWordHere');'");
 
-const NUM_TRIES = 5;
+const NUM_TRIES = 7;
 const NUM_LETTERS = 6;
 const CANVAS_WIDTH = 500;
 const TILE_SIZE = 60;
@@ -12,12 +12,13 @@ const TOP_BOUND = 20;
 
 
 // get list of valid words
-readTextFile("./6_letter_words.txt");
-const VALID_WORDS_ = document.getElementById("valid_words").innerHTML.split(" ");
+//readTextFile("./6_letter_words.txt");
+//const VALID_WORDS_ = document.getElementById("valid_words").innerHTML.split(" ");
 // get list of valid secret words
-readTextFile("./6_letter_secret_words.txt");
-const VALID_SECRET_WORDS = document.getElementById("valid_words").innerHTML.split("\n    ");
+//readTextFile("./6_letter_secret_words.txt");
+//const VALID_SECRET_WORDS = document.getElementById("valid_words").innerHTML.split("\n    ");
 const VALID_WORDS = VALID_WORDS_.concat(VALID_SECRET_WORDS);
+console.log(VALID_WORDS);
 
 var SECRET_WORD;
 SECRET_WORD = randomizeWord();
